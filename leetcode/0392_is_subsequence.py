a2ae3c8fca8f4fd6fdf729fd0b67f6b778132d13
Mellanfrost <mellanfrost@gmail.com>
2024-09-12 21:38:15 +0200

    leetcode 392 solution 1

---
+++ leetcode/0392_is_subsequence.py
@@ -0,0 +1,66 @@
+from tester import test
+
+
+# 392. Is Subsequence
+# https://leetcode.com/problems/is-subsequence
+
+
+"""
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none)
+of the characters without disturbing the relative positions of the remaining characters.
+(i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+Example 1:
+Input: s = "abc", t = "ahbgdc"
+Output: true
+
+Example 2:
+Input: s = "axc", t = "ahbgdc"
+Output: false
+ 
+Constraints:
+0 <= s.length <= 100
+0 <= t.length <= 104
+s and t consist only of lowercase English letters.
+"""
+
+
+# move forward recursively
+def solution_one(s:str, t:str) -> bool:
+    if s == "":
+        return True
+    if t == "":
+        return False
+    def f(i, j):
+        if i == len(s)-1 and s[i] == t[j]:
+            return True
+        if j == len(t)-1:
+            return False
+        if s[i] == t[j]:
+            i+=1
+            j+=1
+        else:
+            j+=1
+        return f(i, j)
+    return f(0, 0)
+
+
+# TESTING
+# --------------------
+
+test_cases = [
+    [["abc", "abcde"], True],
+    [["aec", "abcde"], False],
+    [["", "abcde"], True],
+    [["abc", ""], False],
+    [["a", "b"], False],
+    [["b", "a"], False],
+    [["aa", "aa"], True],
+    [["ab", "ba"], False],
+    [["ba", "ab"], False],
+    [["asd", "asfg"*100+"d"], True],
+]
+
+test(solution_one, test_cases)
