e041f020fb80254d933be6dd83c72afba38a2005
Mellanfrost <mellanfrost@gmail.com>
2024-09-16 22:35:20 +0200

    leetcode 13 solution 2

--- leetcode/0013_roman_to_integer.py
+++ leetcode/0013_roman_to_integer.py
@@ -26,7 +26,6 @@ However, the numeral for four is not IIII. Instead, the number four is written a
 Because the one is before the five we subtract it making four.
 The same principle applies to the number nine, which is written as IX.
 
-
 There are six instances where subtraction is used:
 I can be placed before V (5) and X (10) to make 4 and 9. 
 X can be placed before L (50) and C (100) to make 40 and 90. 
@@ -75,6 +74,29 @@ def solution_one(s:str) -> int:
     return result
 
 
+def solution_two(s:str) -> int:
+    numerals = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000,
+    }
+
+    result = 0
+    s = s.replace("IV", "IIII")
+    s = s.replace("IX", "VIIII")
+    s = s.replace("XL", "XXXX")
+    s = s.replace("XC", "LXXXX")
+    s = s.replace("CD", "CCCC")
+    s = s.replace("CM", "DCCCC")
+    for char in s:
+        result += numerals[char]
+    return result
+
+
 # TESTING
 # --------------------
 
@@ -85,3 +107,4 @@ test_cases = [
 ]
 
 test(solution_one, test_cases)
+test(solution_two, test_cases)
